c631a1bbd6e52932f7e14bd43abac585af78a7d7
Rain Liu <yliu@live.com>
2014-03-07 22:37:48 -0800

    pass ch7/atomic

--- demo/ch7/atomic/atomic.go
+++ demo/ch7/atomic/atomic.go
@@ -1,175 +1,94 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define PROGRAM_FILE "atomic.cl"
-#define KERNEL_FUNC "atomic"
+package main
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+import (
+   "fmt"
+   "gocl/cl"
+   "unsafe"
+   "gocl/demo/ch0"
+)
 
-#ifdef MAC
-#include <OpenCL/cl.h>
-#else
-#include <CL/cl.h>
-#endif
+const PROGRAM_FILE =       "atomic.cl"
+var    KERNEL_FUNC =[]byte("atomic")
 
-/* Find a GPU or CPU associated with the first available platform */
-cl_device_id create_device() {
-
-   cl_platform_id platform;
-   cl_device_id dev;
-   int err;
-
-   /* Identify a platform */
-   err = clGetPlatformIDs(1, &platform, NULL);
-   if(err < 0) {
-      perror("Couldn't identify a platform");
-      exit(1);
-   } 
-
-   /* Access a device */
-   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
-   if(err == CL_DEVICE_NOT_FOUND) {
-      err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
-   }
-   if(err < 0) {
-      perror("Couldn't access any devices");
-      exit(1);   
-   }
-
-   return dev;
-}
-
-/* Create program from a file and compile it */
-cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename) {
-
-   cl_program program;
-   FILE *program_handle;
-   char *program_buffer, *program_log;
-   size_t program_size, log_size;
-   int err;
-
-   /* Read program file and place content into buffer */
-   program_handle = fopen(filename, "r");
-   if(program_handle == NULL) {
-      perror("Couldn't find the program file");
-      exit(1);
-   }
-   fseek(program_handle, 0, SEEK_END);
-   program_size = ftell(program_handle);
-   rewind(program_handle);
-   program_buffer = (char*)malloc(program_size + 1);
-   program_buffer[program_size] = '\0';
-   fread(program_buffer, sizeof(char), program_size, program_handle);
-   fclose(program_handle);
-
-   /* Create program from file */
-   program = clCreateProgramWithSource(ctx, 1, 
-      (const char**)&program_buffer, &program_size, &err);
-   if(err < 0) {
-      perror("Couldn't create the program");
-      exit(1);
-   }
-   free(program_buffer);
-
-   /* Build program */
-   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-   if(err < 0) {
-
-      /* Find size of log and print to std output */
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            0, NULL, &log_size);
-      program_log = (char*) malloc(log_size + 1);
-      program_log[log_size] = '\0';
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            log_size + 1, program_log, NULL);
-      printf("%s\n", program_log);
-      free(program_log);
-      exit(1);
-   }
-
-   return program;
-}
-
-int main() {
+func main() {
 
    /* OpenCL data structures */
-   cl_device_id device;
-   cl_context context;
-   cl_command_queue queue;
-   cl_program program;
-   cl_kernel kernel;
-   cl_int err;
-   size_t offset, global_size, local_size;
+   var device []cl.CL_device_id;
+   var context cl.CL_context;
+   var queue cl.CL_command_queue;
+   var program *cl.CL_program;
+   var kernel cl.CL_kernel;
+   var err cl.CL_int;
+
+   var offset, global_size, local_size [1]cl.CL_size_t;
 
    /* Data and events */
-   int data[2];
-   cl_mem data_buffer;
+   var data [2]cl.CL_int;
+   var data_buffer cl.CL_mem;
 
    /* Create a device and context */
-   device = create_device();
-   context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
-   if(err < 0) {
-      perror("Couldn't create a context");
-      exit(1);   
+   device = ch0.Create_device();
+   context = cl.CLCreateContext(nil, 1, device[:], nil, nil, &err);
+   if err < 0  {
+      println("Couldn't create a context");
+      return   
    }     
 
    /* Build the program and create a kernel */
-   program = build_program(context, device, PROGRAM_FILE);
-   kernel = clCreateKernel(program, KERNEL_FUNC, &err);
-   if(err < 0) {
-      perror("Couldn't create a kernel");
-      exit(1);   
+   program = ch0.Build_program(context, device[:], PROGRAM_FILE, nil);
+   kernel = cl.CLCreateKernel(*program, KERNEL_FUNC, &err);
+   if err < 0  {
+      println("Couldn't create a kernel");
+      return   
    };
 
    /* Create a buffer to hold data */
-   data_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-      sizeof(data), NULL, &err);
-   if(err < 0) {
-      perror("Couldn't create a buffer");
-      exit(1);   
+   data_buffer = cl.CLCreateBuffer(context, cl.CL_MEM_WRITE_ONLY, 
+      cl.CL_size_t(unsafe.Sizeof(data[0]))*2, nil, &err);
+   if err < 0  {
+      println("Couldn't create a buffer");
+      return   
    };         
 
    /* Create kernel argument */
-   err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &data_buffer);
-   if(err < 0) {
-      perror("Couldn't set a kernel argument");
-      exit(1);   
+   err = cl.CLSetKernelArg(kernel, 0, cl.CL_size_t(unsafe.Sizeof(data_buffer)), unsafe.Pointer(&data_buffer));
+   if err < 0  {
+      println("Couldn't set a kernel argument");
+      return   
    };
 
    /* Create a command queue */
-   queue = clCreateCommandQueue(context, device, 0, &err);
-   if(err < 0) {
-      perror("Couldn't create a command queue");
-      exit(1);   
+   queue = cl.CLCreateCommandQueue(context, device[0], 0, &err);
+   if err < 0  {
+      println("Couldn't create a command queue");
+      return   
    };
 
    /* Enqueue kernel */
-   offset = 0;
-   global_size = 8;
-   local_size = 4;
-   err = clEnqueueNDRangeKernel(queue, kernel, 1, &offset, &global_size, &local_size, 0, NULL, NULL);
-   if(err < 0) {
-      perror("Couldn't enqueue the kernel");
-      exit(1);   
+   offset[0] = 0;
+   global_size[0] = 8;
+   local_size[0] = 4;
+   err = cl.CLEnqueueNDRangeKernel(queue, kernel, 1, offset[:], global_size[:], local_size[:], 0, nil, nil);
+   if err < 0  {
+      println("Couldn't enqueue the kernel");
+      return   
    }
 
    /* Read the buffer */
-   err = clEnqueueReadBuffer(queue, data_buffer, CL_TRUE, 0, 
-      sizeof(data), data, 0, NULL, NULL);
-   if(err < 0) {
-      perror("Couldn't read the buffer");
-      exit(1);
+   err = cl.CLEnqueueReadBuffer(queue, data_buffer, cl.CL_TRUE, 0, 
+      cl.CL_size_t(unsafe.Sizeof(data[0]))*2, unsafe.Pointer(&data[0]), 0, nil, nil);
+   if err < 0  {
+      println("Couldn't read the buffer");
+      return
    }
 
-   printf("Increment: %d\n", data[0]);
-   printf("Atomic increment: %d\n", data[1]);
+   fmt.Printf("Increment: %d\n", data[0]);
+   fmt.Printf("Atomic increment: %d\n", data[1]);
 
    /* Deallocate resources */
-   clReleaseMemObject(data_buffer);
-   clReleaseKernel(kernel);
-   clReleaseCommandQueue(queue);
-   clReleaseProgram(program);
-   clReleaseContext(context);
-   return 0;
+   cl.CLReleaseMemObject(data_buffer);
+   cl.CLReleaseKernel(kernel);
+   cl.CLReleaseCommandQueue(queue);
+   cl.CLReleaseProgram(*program);
+   cl.CLReleaseContext(context);
 }
