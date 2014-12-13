58fa807212e1e48bf19b4f5ca650ca849447e6fa
Rain Liu <yuliu@outlook.com>
2014-12-12 23:25:35 -0800

    add cl11 Enqueue

---
+++ cl/event11.go
@@ -0,0 +1,64 @@
+// +build cl11
+
+package cl
+
+/*
+#cgo CFLAGS: -I CL
+#cgo !darwin LDFLAGS: -lOpenCL
+#cgo darwin LDFLAGS: -framework OpenCL
+
+#include "CL/opencl.h"
+*/
+import "C"
+
+///////////////////////////////////////////////
+//OpenCL 1.1
+///////////////////////////////////////////////
+
+func CLEnqueueMarker(command_queue CL_command_queue, event *CL_event) CL_int {
+	var c_event C.cl_event
+	var c_errcode_ret C.cl_int
+
+	c_errcode_ret = C.clEnqueueMarker(command_queue.cl_command_queue, &c_event)
+
+	if event != nil {
+		event.cl_event = c_event
+	}
+
+	return CL_int(c_errcode_ret)
+}
+
+func CLEnqueueBarrier(command_queue CL_command_queue) CL_int {
+	return CL_int(C.clEnqueueBarrier(command_queue.cl_command_queue))
+}
+
+func CLEnqueueWaitForEvents(command_queue CL_command_queue,
+	num_events_in_wait_list CL_uint,
+	event_wait_list []CL_event) CL_int {
+
+	if (num_events_in_wait_list == 0 && event_wait_list != nil) ||
+		(num_events_in_wait_list != 0 && event_wait_list == nil) ||
+		int(num_events_in_wait_list) != len(event_wait_list) {
+		return CL_INVALID_EVENT_WAIT_LIST
+	}
+
+	var c_errcode_ret C.cl_int
+
+	if num_events_in_wait_list != 0 {
+		var c_event_wait_list []C.cl_event
+		c_event_wait_list = make([]C.cl_event, num_events_in_wait_list)
+		for i := 0; i < int(num_events_in_wait_list); i++ {
+			c_event_wait_list[i] = event_wait_list[i].cl_event
+		}
+
+		c_errcode_ret = C.clEnqueueWaitForEvents(command_queue.cl_command_queue,
+			C.cl_uint(num_events_in_wait_list),
+			&c_event_wait_list[0])
+	} else {
+		c_errcode_ret = C.clEnqueueWaitForEvents(command_queue.cl_command_queue,
+			0,
+			nil)
+	}
+
+	return CL_int(c_errcode_ret)
+}
--- ocl/queue11.go
+++ ocl/queue11.go
@@ -2,6 +2,47 @@
 
 package ocl
 
+import (
+	"errors"
+	"gocl/cl"
+)
+
 type CommandQueue interface {
 	queue1x
+
+	//cl11
+	EnqueueMarker() (Event, error)
+	EnqueueBarrier() error
+	EnqueueWaitForEvents(event_wait_list []Event) error
+}
+
+func (this *command_queue) EnqueueMarker() (Event, error) {
+	var event_id cl.CL_event
+	if errCode := cl.CLEnqueueMarker(this.command_queue_id, &event_id); errCode != cl.CL_SUCCESS {
+		return nil, errors.New("EnqueueMarker failure with errcode_ret " + string(errCode))
+	} else {
+		return &event{event_id}, nil
+	}
+}
+
+func (this *command_queue) EnqueueBarrier() error {
+	if errCode := cl.CLEnqueueBarrier(this.command_queue_id); errCode != cl.CL_SUCCESS {
+		return errors.New("EnqueueBarrier failure with errcode_ret " + string(errCode))
+	} else {
+		return nil
+	}
+}
+
+func (this *command_queue) EnqueueWaitForEvents(event_wait_list []Event) error {
+	numEvents := cl.CL_uint(len(event_wait_list))
+	events := make([]cl.CL_event, numEvents)
+	for i := cl.CL_uint(0); i < numEvents; i++ {
+		events[i] = event_wait_list[i].GetID()
+	}
+
+	if errCode := cl.CLEnqueueWaitForEvents(this.command_queue_id, numEvents, events); errCode != cl.CL_SUCCESS {
+		return errors.New("EnqueueWaitForEvents failure with errcode_ret " + string(errCode))
+	} else {
+		return nil
+	}
 }
